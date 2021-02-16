2448eabf656961ce636bc8b1d0b744b5f19a93f9
Stephen Downes <stephen@downes.ca>
2021-02-16 11:27:57 -0500

    Full directory on update.sh

--- cgi-bin/update/update.sh
+++ cgi-bin/update/update.sh
@@ -4,8 +4,9 @@ repository="https://github.com/Downes/gRSShopper"
 repoFolder="./update/repofolder"
 repoCGI="./update/repofolder/cgi-bin/*"
 backup="./backup"
-cp -R ../cgi-bin/* $backup
+cp -R /var/www/html/cgi-bin/* $backup
 rm -R -f $repoFolder
 git clone $repository $repoFolder
-cp -R $repoCGI ../cgi-bin
+cp -R $repoCGI /var/www/html/cgi-bin
+chmod 775 /var/www/html/cgi-bin/update.sh
 echo "Updated CGI"
