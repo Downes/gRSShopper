4dfb1933d47c2e3bf3b14cff1fb2499c5f4ec0f6
Stephen Downes <stephen@downes.ca>
2021-02-16 11:37:14 -0500

    Now updates assets/js as well

--- cgi-bin/update/update.sh
+++ cgi-bin/update/update.sh
@@ -3,10 +3,14 @@
 repository="https://github.com/Downes/gRSShopper"
 repoFolder="./update/repofolder"
 repoCGI="./update/repofolder/cgi-bin/*"
-backup="./backup"
-cp -R /var/www/html/cgi-bin/* $backup
+repoJS="./update/repofolder/html/assets/js*"
+cgbackup="./cgbackup"
+cgbackup="./jsbackup"
+cp -R /var/www/html/cgi-bin/* $cgbackup
+cp -R /var/www/html/cgi-bin/* $jsbackup
 rm -R -f $repoFolder
 git clone $repository $repoFolder
 cp -R $repoCGI /var/www/html/cgi-bin
+cp -R $repoJS /var/www/html/assets/js
 chmod 775 /var/www/html/cgi-bin/update/update.sh
 echo "Updated CGI"
