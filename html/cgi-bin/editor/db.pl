b4a5cc995f8b1efd1921fe3368b022ad059390ed
Stephen Downes <stephen@downes.ca>
2022-03-21 11:46:37 -0400

    Debugging

--- html/cgi-bin/editor/db.pl
+++ html/cgi-bin/editor/db.pl
@@ -1006,7 +1006,7 @@ sub db_update {
 	my $sth = $dbh->prepare($sql);
 
 	if ($diag eq "on") { print "$sql <br/>\n @sqlv <br/>\n"; }
-    	$sth->execute(@sqlv) or &error($dbh,"","","Updatde failed: ".$sth->errstr);
+    	$sth->execute(@sqlv) or &status_error("Update failed: ".$sth->errstr);
 
 	return $where;
 
